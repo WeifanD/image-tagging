67f86ad94c81739048645a0703c95e3690051bc0
bdai <bdai_decathlon_windows>
2020-03-03 11:29:49 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#
+# NAME: content-generation
+THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
+THIS_FILE=$(basename "$0")
+THIS_PATH="$THIS_DIR/$THIS_FILE"
+echo "`date` Info: This file $THIS_FILE start running on $THIS_DIR..."
+
+source env/bin/activate
+echo "run main_text.py for text"
+python main_text.py
+
+echo "run main.py for img"
+python main.py
