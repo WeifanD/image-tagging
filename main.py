8c3d5b8ff0958b30a70003320eee7950eec3962b
bdai <bdai_decathlon_windows>
2020-03-12 10:11:59 +0800

    aa

--- main.py
+++ main.py
@@ -94,5 +94,5 @@ db_send_update_from_file(conn, "src/data/SQL/create_image_table.sql")
 for json in df_json:
 	print(json)
 	db_send_update_from_file(conn, "src/data/SQL/insert_image_data.sql", json)
-print(pd.read_sql_query(con = conn, sql = 'select * from bi.d_content_img;'))
+# print(pd.read_sql_query(con = conn, sql = 'select * from bi.d_content_img;'))
 db_send_update_from_file(conn, "src/data/SQL/update_content_img.sql")
